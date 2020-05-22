2f430694267e4f7443b696001706241112f5f4df
周峰 <zhoufeng12@meituan.com>
2020-05-22 13:58:52 +0800

    原子性与易变性练习；

---
+++ src/main/java/concurrency/SerialNumberGenerator.java
@@ -0,0 +1,9 @@
+package concurrency;
+
+public class SerialNumberGenerator {
+    private static volatile int serialNumber = 0;
+
+    public static int nextSerialNumber() {
+        return serialNumber++;  // Not thread-safe
+    }
+}
